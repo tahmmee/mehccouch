e5b027dcd948a7bb85c16c6f29692f4805bcea53
Tommie McAfee <tommie@couchbase.com>
2015-11-30 11:43:40 -0500

    more guarding against uninstall errors

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@ ps aux | grep couchbase && ps aux | grep couchbase | awk '{print $2}' | xargs -I
 sleep 1
 ps aux | grep couchbase && ps aux | grep couchbase | awk '{print $2}' | xargs -I '{}' kill -9 '{}'
 cd
-apt-get remove -y couchbase-server  || true
+apt-get remove -y couchbase-server 2>/dev/null  || true
 rm -rf /opt/couchbase && rm -rf /data/*
 dpkg --purge --force-all couchbase-server
 sleep 3
 ps aux | grep couchbase && ps aux | grep couchbase | awk '{print $2}' | xargs -I '{}' kill -9 '{}'
 sleep 1
-apt-get remove -y couchbase-server
+apt-get remove -y couchbase-server 2>/dev/null || true
+
 wget http://172.23.120.24/builds/latestbuilds/couchbase-server/watson/$1/couchbase-server-enterprise_4.5.0-$1-ubuntu14.04_amd64.deb
 dpkg -i couchbase-server-enterprise_4.5.0-$1-ubuntu14.04_amd64.deb
